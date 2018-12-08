5693ef8fba9dd3565e9fb647a452450ed2fe21da
pvienhage <pevienhage@gmail.com>
2018-12-08 02:40:24 -0500

    intermediate

--- client/app.js
+++ client/app.js
@@ -2,31 +2,48 @@ var express = require('express');
 var http = require('http');
 const Store = require('data-store');
 
-const store = new Store({ path: 'config.json' });
-store.set("hash", process.argv[2])
-store.set("room", process.argv[3])
+const lib = require('../lib/lib.js')
 
-var app = express();
-var server = http.createServer(app);
-
-app.get('/', function(req, res) {
-  res.send("Hello World!");
-});
-
-app.get('/getBlockNumber', function(req, res){
-  
+lib((error, api) => {
+  if (error) {
+    console.log(error)
+    process.exit()
+  } else{
+    main()
+  }
 })
 
-app.get('/getRoom', function(req, res) {
-  res.send(store.get("room"));
-});
+function main(){
+  const store = new Store({ path: 'config.json' });
+  store.set("hash", process.argv[2])
+  store.set("topic", process.argv[3])
 
-app.get('/getRootHash', function(req,res){
-  res.send(store.get("hash"))
-})
+  var app = express();
+  var server = http.createServer(app);
+
+  app.get('/', function(req, res) {
+    res.send("Hello World!");
+  });
+
+  app.get('/getBlockNumber', function(req, res){
+    res.send(api.getBlockNumber(store.get("hash")))
+  })
+
+  app.get('/getTopic', function(req, res) {
+    res.send(store.get("topic"));
+  });
+
+  app.get('/getRootHash', function(req, res){
+    res.send(api.getRootHash(store.get("topic")))
+  })
+
+  app.post('/changeTopic', function(req, res){
+   api.changeTopic(req.query.topic)
+  })
 
 
-server.listen(3000, 'localhost');
-server.on('listening', function() {
-    console.log('Express server started on port %s at %s', server.address().port, server.address().address);
-});
+  server.listen(3000, 'localhost');
+  server.on('listening', function() {
+      console.log('Express server started on port %s at %s', server.address().port, server.address().address);
+  });
+}
