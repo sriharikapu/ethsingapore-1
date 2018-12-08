6e9c7bc6094fd970ebd5846018124ccc9a6835e7
James Towle <jalextowle@berkeley.edu>
2018-12-08 22:27:43 +0800

    Added the _addBlock method

--- authority/authority.js
+++ authority/authority.js
@@ -8,9 +8,13 @@ module.exports = (topic, root, endpoint) => {
     return new Authority(topic, root, endpoint)
 }
 
+
 class Authority extends EventEmitter {
 
-    constructor (topic, root, endpoint) {
+    constructor (topic, root, endpoints) {
+        console.log(endpoints.begin)
+        console.log(endpoints.end)
+        console.log(endpoints.endpoint)
         super()
         // Start IPFS node with experimental pubsub on
         this._node = new IPFS({
@@ -25,8 +29,11 @@ class Authority extends EventEmitter {
                 }
             }
         })
+        this._root = root
         this._topic = topic
-        this._endpoint = endpoint
+        this._endpoint = endpoints.endpoint
+        this._begin = endpoints.begin
+        this._end = endpoints.end
         this._start(root)
     }
 
@@ -79,32 +86,73 @@ class Authority extends EventEmitter {
                 // console.log(blockNum)
                 // console.log('cur: ' + this.lastBlock())
                 if (blockNum > this.lastBlock()) {
-                    this._addFile(blockNum, (err, cur, cid) => {
-                        if (err) {
-                            this.emit('error', err)
+                    this._addBlock(blockNum, (block_err, block_cur, block_cid) => {
+                        if (block_err) {
+                            this.emit('error', block_err)
                         } else {
-                            this._root = cid.toString()
-                            this._curFile = cur
-                            this.emit('file added', cur, cid)
+                            this.emit('block added', block_cur)
                         }
+                        this._addFile(blockNum, block_cid.toString(), (err, cur, cid) => {
+                            if (err) {
+                                this.emit('error', err)
+                            } else {
+                                this._root = cid.toString()
+                                this._curFile = cur
+                                this.emit('file added', cur, cid)
+                            }
+                        })
                     })
                 }
             }
         })
     }
 
-    _addFile (blockNum, callback) {
+    _addFile (blockNum, block_cid, callback) {
         console.log('Adding file: ' + blockNum)
         var cur = this._curFile
         cur.latest = blockNum
         if (cur.blocks.length == 256) {
             cur.last = this._root
+            cur.blocks = [ block_cid ]
         } else {
-            cur.blocks.push(this._root)
+            console.log(this._root)
+            cur.blocks.push(block_cid)
         }
         this._node.dag.put(cur, KECCAK_JSON, function (err, cid) {
             callback(err, cur, cid)
         })
+    }
+
+    _addBlock (blockNum, callback) {
+        console.log(blockNum)
+        console.log(this._toHex(blockNum))
+        let endpoint = this._begin + this._toHex(blockNum) + this._end
+        console.log(endpoint)
+        request(endpoint, (err, resp, body) => {
+           if (err) {
+               callback(err)
+           } else {
+               this._node.dag.put(JSON.parse(body), KECCAK_JSON, (e, cid) => {
+                   callback(e, JSON.parse(body), cid)
+               })
+           }
+        })
+    }
+
+    _toNibble(val) {
+      if (val < 10) {
+        return String.fromCharCode(val + 48)
+      } else {
+        return String.fromCharCode(val + 87)
+      }
+    }
+
+    _toHex(val) {
+      if (val < 16) {
+        return this._toNibble(val) 
+      } else {
+        return this._toHex(val / 16) + this._toNibble(val % 16)
+      }
     }
 
     lastBlock () {
--- authority/index.js
+++ authority/index.js
@@ -6,6 +6,8 @@ program
     .parse(process.argv)
     
 const BLOCKNUM_ENDPOINT = "https://api.etherscan.io/api?module=proxy&action=eth_blockNumber&apikey=EEQ2VBSYD5D4UQM7GH5DBD1YQTNPWUD8R5"
+const BLOCK_ENDPOINT_BEGIN = "https://api.etherscan.io/api?module=proxy&action=eth_getBlockByNumber&tag=" 
+const BLOCK_ENDPOINT_END = "boolean=true&apikey=EEQ2VBSYD5D4UQM7GH5DBD1YQTNPWUD8R5" 
 const DEFAULT_ROOT = "zdq6yJpRc1YzdMXgaa1irZaRThvqNd9vThiVqWqDBRuCMakPD"
 const DEFAULT_TOPIC = "ethsg"
 const INITIAL_ROOT = program.root ? program.root : DEFAULT_ROOT
@@ -13,7 +15,7 @@ const INITIAL_TOPIC = program.topic ? program.topic : DEFAULT_TOPIC
 
 // Start IPFS node
 const Authority = require('./authority.js')
-const authority = Authority(INITIAL_TOPIC, INITIAL_ROOT, BLOCKNUM_ENDPOINT)
+const authority = Authority(INITIAL_TOPIC, INITIAL_ROOT, { begin: BLOCK_ENDPOINT_BEGIN, end: BLOCK_ENDPOINT_END, endpoint: BLOCKNUM_ENDPOINT })
 
 authority.on('node ready', () => {
     
@@ -32,6 +34,10 @@ authority.on('node ready', () => {
     authority.on('file added', (file, newRoot) => {
         console.log('Added file: ' + JSON.stringify(file))
         console.log('New root: ' + newRoot)
+    })
+
+    authority.on('block added', (file) => {
+        console.log('Added file: ' + JSON.stringify(file))
     })
 
     authority.on('peer joined', (peer) => {
