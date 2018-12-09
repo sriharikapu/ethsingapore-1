2d1b5099c483459bba50d9f3f875100133a342e6
pvienhage <pevienhage@gmail.com>
2018-12-08 23:48:21 -0500

    Files match current master

---
+++ authority/authority.js
@@ -0,0 +1,207 @@
+const requests = require('request')
+const util = require('util')
+const request = util.promisify(requests)
+const Room = require('ipfs-pubsub-room')
+const IPFS = require('ipfs')
+const EventEmitter = require('events')
+const KECCAK_JSON = { format: 'dag-cbor', hashAlg: 'keccak-256' }
+
+module.exports = (topic, root, endpoint) => {
+    return new Authority(topic, root, endpoint)
+}
+
+
+class Authority extends EventEmitter {
+
+    constructor (topic, root, endpoints) {
+        super()
+        // Start IPFS node with experimental pubsub on
+        this._node = new IPFS({
+            EXPERIMENTAL: {
+                pubsub: true
+            },
+            config: {
+                Addresses: {
+                    Swarm: [
+                        '/dns4/ws-star.discovery.libp2p.io/tcp/443/wss/p2p-websocket-star'
+                    ]
+                }
+            }
+        })
+        this._root = root
+        this._topic = topic
+        this._endpoint = endpoints.endpoint
+        this._begin = endpoints.begin
+        this._end = endpoints.end
+    }
+
+    async start() {
+        // console.log('start')
+        this._node.on('ready', async () => {
+            this.emit('node ready')
+
+            this._room = Room(this._node, this._topic)
+
+            this._room.on('subscribed', () => {
+                this.emit('subscribed')
+            })
+            
+            this._room.on('peer joined', (peer) => {
+                this.emit('peer joined', peer)
+                this._room.broadcast(this._root)
+            })
+
+            this._room.on('error', (err) => {
+                this.emit('error', err)
+            })
+
+            var res = await this._initialize(this._root)
+            if (!res.err) {
+                // console.log('interval')
+                setInterval(this._update.bind(this), 1000)
+            }
+        })
+    }
+
+    async _initialize (root) {
+        // console.log('initialize')
+        var res = await this._node.dag.get(root)
+        if (res.err) {
+            this.emit('error', res.err)
+        } else {
+            this._curFile = res.value
+            this.emit('ready')
+        }
+        return res
+    }
+
+    async _update () {
+        if (this._isUpdating) {
+            return
+        }
+        this._isUpdating = true
+        // console.log('updating')
+        
+        var blockNum = await request(this._endpoint)
+        if (blockNum.err) {
+            this.emit('error', blockNum.err)
+            this._isUpdating = false
+            return
+        }
+
+        blockNum = parseInt(JSON.parse(blockNum.body).result, 16)
+        if (blockNum <= this.lastBlock()) {
+            this._isUpdating = false
+            return
+        }
+
+        var blockCid = await this._addBlock(blockNum)
+        if (blockCid.err) {
+            this.emit('error', blockCid.err)
+            this._isUpdating = false
+            return
+        }
+        blockCid = blockCid.toString()
+        
+        var fileCid = await this._addFile(blockNum, blockCid)
+        if (fileCid.err) {
+            this.emit('error', fileCid.err)
+            this._isUpdating = false
+            return
+        }
+        this._isUpdating = false
+    }
+
+    async _addBlock (blockNum) {
+        console.log('Adding block')
+        let endpoint = this._begin + this._toHex(blockNum) + this._end
+        var response = await request(endpoint)
+        if (response.err) {
+            return response
+        }
+        
+        var body = JSON.parse(response.body)
+        var txCid = await this._node.dag.put(body.result.transactions, KECCAK_JSON)
+        if (!txCid.err) {
+            this.emit('block added', body)
+        }
+      
+        let obj = {
+            jsonrpc: body.jsonrpc, 
+            id: body.id,
+            difficulty: body.result.difficulty, 
+            extraData: body.result.extraData,
+            gasLimit: body.result.gasLimit,
+            gasUsed: body.result.gasUsed,
+            hash: body.result.hash,
+            logsBloom: body.result.logsBloom,
+            miner: body.result.miner,
+            mixHash: body.result.mixHash,
+            nonce: body.result.nonce,
+            number: body.result.number,
+            parentHash: body.result.parentHash,
+            receiptsRoot: body.result.receiptsRoot,
+            sha3Uncles: body.result.sha3Uncles,
+            size: body.result.size,
+            stateRoot: body.result.stateRoot,
+            timestamp: body.result.timestamp,
+            totalDifficulty: body.result.totalDifficulty,
+            transactions: txCid.toString()
+        }
+        var blockCid = await this._node.dag.put(obj, KECCAK_JSON)
+        if (blockCid.err) {
+            this.emit('error', blockCid.err) 
+        }
+
+        return blockCid
+    }
+
+    async _addFile (blockNum, blockCid) {
+        console.log('Adding file: ' + blockNum)
+        var cur = this._curFile
+        cur.latest = blockNum
+        if (cur.blocks.length == 256) {
+            cur.last = this._root
+            cur.blocks = [blockCid]
+        } else {
+            cur.blocks.push(blockCid)
+        }
+
+        var fileCid = await this._node.dag.put(cur, KECCAK_JSON)
+        if (!fileCid.err) {
+            this._curFile = cur
+            this._root = fileCid.toString()
+            this.emit('file added', cur, fileCid)
+        }
+
+        return fileCid
+    }
+
+    _toNibble(val) {
+      if (val < 10) {
+        return String.fromCharCode(val + 48)
+      } else {
+        return String.fromCharCode(val + 87)
+      }
+    }
+
+    _toHex(val) {
+      if (val < 16) {
+        return this._toNibble(val) 
+      } else {
+        return this._toHex(val / 16) + this._toNibble(val % 16)
+      }
+    }
+
+    lastBlock () {
+        return this._curFile.latest
+    }
+
+    getTopic () {
+        return this._topic
+    }
+
+    getFiles () {
+        return this._curFile
+    }
+}
--- client/app.js
+++ client/app.js
@@ -1,15 +1,16 @@
-var express = require('express');
-var http = require('http');
+const express = require('express');
+const http = require('http');
 const Store = require('data-store');
-var path = require('path');
+const path = require('path');
 const lib = require('../lib/lib.js');
+const body_parser = require('body-parser')
 
 lib((error, api) => {
   if (error) {
     console.log(error)
     process.exit()
   } else{
-     main(api);
+    main(api);
   }
 })
 
@@ -18,19 +19,20 @@ function main(api) {
   store.set("hash", process.argv[2])
   store.set("topic", process.argv[3])
 
-  var app = express();
-  var server = http.createServer(app);
+  var app = express()
+  app.use(body_parser.json())
+  app.use(body_parser.urlencoded({ extended: true }))
 
   app.get('/', function(req, res) {
      res.sendFile(path.join(__dirname + '/index.html'));
-  });
+  })
 
-  app.get('/getBlockNumber', function(req, res){
-    api.getBlockNumber(store.get('hash'), (error, number) => {
-    //api.getBlockNumber("zdpuB1tmv8KF6We446PW2fHccQipc7GHKuSgPfHEjuqgPxEBu", (error, number) =>{
+  app.get('/getBlockNumber', function(req, res) {
+    api.getBlockNumber(store.get('hash'), (error, number) =>{
       if(error){
         console.log(error)
       } else{
+  
         res.json(number)
       }
     })
@@ -40,55 +42,62 @@ function main(api) {
     res.send(store.get("topic"));
   });
 
-  app.get('/getRootHash', function(req, res){
-    var hash = api.getRootHash(store.get("topic"))
+  app.get('/getRootHash', function(req, res) {
+    var hash = api.getRootHash()
     res.send(hash)
-    store.set("hash", hash)
   })
 
-  app.get('/getBlockByNumber', function(req, res){
-    api.getBlockByNumber(store.get("hash") , req.query.number, (error, block) => {
-      if(error){
+  app.get('/getBlockByNumber', function (req, res) {
+    console.log(store.get('hash'))
+    console.log(req.query.blocknumber)
+    console.log(req.query.tx)
+    api.getBlockByNumber(store.get('hash'), req.query.blocknumber, req.query.tx, (error, block) => {
+      if (error) {
         console.log(error)
-      } else{
+      } else {
         res.send(block)
       }
     })
   })
 
-
-  app.get('/getBlockTransactionCountByNumber', function(req, res){
-    api.getBlockTransactionCountByNumber(store.get("hash"), req.query.number, (error, count) => {
-      if(error){
+  app.get('/getBlockTrasactionCount', function (req, res) {
+    api.getBlockByNumber(store.get('topic'), req.query.blocknumber, (error, count) => {
+      if (error) {
         console.log(error)
-      } else{
+      } else {
         res.send(count)
       }
     })
   })
 
-  app.get('/gasPrice', function(req, res){
-    api.getGasPrice(store.get("hash"), (error, price) =>{
-      if(error){
-        console.log(error)
-      } else{
-        res.send(price)
-      }
-    })
+  app.post('/changeAuthority', function (req, res) {
+    api.changeAuthority(req.query.authority) 
   })
 
-  app.post('/changeTopic', function(req, res){
+  app.post('/changeRoot', function (req, res) {
+    api.changeRoot(req.body.roothash)
+    store.set('hash', req.body.roothash)
+    res.redirect('/')
+  })
+
+  app.post('/changeTopic', function(req, res) {
    api.changeTopic(req.query.topic, error => {
      if (error) {
        console.log(error)
      } else {
-        store.set("topic",req.query.topic)
+        store.set("topic", req.query.topic)
      }
    })
   })
 
+  app.listen(3000, () => {
+    console.log('Server listening on port 3000')
+  })
+  /*
+  var server = http.createServer(app);
   server.listen(3000, 'localhost');
   server.on('listening', function() {
       console.log('Express server started on port %s at %s', server.address().port, server.address().address);
   });
+  */
 }
